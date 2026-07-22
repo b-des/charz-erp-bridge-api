9f80c0f061be1aadf4b287c961a3d19dbb3d1eeb
Andrii <alltelex@gmail.com>
2026-07-22 17:48:10 +0300

    add migrations, update workflow

---
+++ src/Migrations/20260721130040_UpdateDefectEntity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CharzPiexApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDefectEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DocumentRef",
+                table: "DefectEntityItems",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DocumentRef",
+                table: "DefectEntityItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
---
+++ src/Migrations/20260721145654_AddFilePathToDefectEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CharzPiexApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFilePathToDefectEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "DefectEntityItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "DefectEntityItems");
+        }
+    }
+}
